5807541588dd8a54f51581b823bb662d9c68120e
sngjss <sunga.jss@gmail.com>
2015-07-30 15:51:11 -0400

    Continued fixing the home page. Bottom portion of the layout has been fixed and need to add more styles. Icons for social buttons have been added. Consider refactoring CSS code to be shorter and cascading. :P

--- template-home.php
+++ template-home.php
@@ -29,55 +29,74 @@ get_header(); ?>
 
 		<section class="video">
 			<div class="video-wrap">
+			<iframe width="650" height="400" src="https://www.youtube.com/embed/RATme3wctPw" frameborder="0" allowfullscreen></iframe>
 			</div>
 			<div class="join-wrap">
-
+			<h1> Why Join? </h1>
+				<ul>
+					<li>You will be making a difference</li>
+					<li>New long-lasting friendships</li>
+					<li>Networking</li>
+					<li>Direct contact with social issues</li>
+					<li>Traveling to new and interesting locations</li>
+					<li>Immerse yourself in a new culture and lifestyle</li>
+					<li>Working together with other schools towards a common goal</li>
+					<li>Build leadership skills for future involvement opportunities</li>
+					<li>Have fun! Give Back!</li>
+				</ul>
 			</div>
 		</section>
 
-		<section class="contact">
-			<div>
-				<span>Volunteer UCF Office:</span> (407) 823-3318
+		<section class="contact-social">
+			<div class="contact-wrap">
+				<h1>Get In Touch With Us!</h1>
+				<div>
+					<span>Volunteer UCF Office:</span> (407) 823-3318
+				</div>
+				<div>
+					<span>Office of Student Involvement Office:</span> (407) 823-6471
+				</div>
+				<div>
+					<span>OSI Fax:</span> (407) 823-5899
+				</div>
+				<div>The Volunteer UCF Cubicle is located in the Office of Student Involvement (SU 208).</div>
+				<div class="vucf-address"> 
+					University of Central Florida
+					<br>
+					Volunteer UCF
+					<br>
+					Attention: ABP
+					<br>
+					PO Box 160245
+					<br>
+					Orlando, FL 32816
+				</div>
 			</div>
-			<div>
-				<span>Office of Student Involvement Office:</span> (407) 823-6471
-			</div>
-			<div>
-				<span>OSI Fax:</span> (407) 823-5899
-			</div>
-			<div>The Volunteer UCF Cubicle is located in the Office of Student Involvement (SU 208).</div>
-			<div class="vucf-address"> 
-				University of Central Florida
-				<br>
-				Volunteer UCF
-				<br>
-				Attention: ABP
-				<br>
-				PO Box 160245
-				<br>
-				Orlando, FL 32816
-			</div>
-		</section>
 
-		<section class="social">
-			<div class="social-btns">
-				<div class="facebook-btn">
-					<a href="#" target="_blank">
-					<img></img>
-					
-					</a>
-				</div>
-				<div class="twitter-btn">
-					<a href="#" target="_blank">
-					<img></img>
-					</a>
-				</div>
-				<div class="instagram-btn">
-					<a href="#" target="_blank">
-					<img></img>
-					</a>
+			<div class="social-wrap">
+				<div class="social-btns">
+					<h1>Socialize!</h1>
+					<div class="facebook-btn">
+						<a href="#" target="_blank">
+						<img></img>
+						<p>Follow us on FACEBOOK</p>
+						</a>
+					</div>
+					<div class="twitter-btn">
+						<a href="#" target="_blank">
+						<img></img>
+						<p>Follow us on TWITTER</p>
+						</a>
+					</div>
+					<div class="instagram-btn">
+						<a href="#" target="_blank">
+						<img></img>
+						<p>Follow us on INSTAGRAM</p>
+						</a>
+					</div>
 				</div>
 			</div>
+
 		</section>
 
 
